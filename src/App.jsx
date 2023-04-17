ab1280ce336884e36b7612d68d124af1501284cc
nateazevedo <nathanielazevedo@hotmail.com>
2023-04-17 00:12:56 -0700

    simple front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { fetchTasks } from "./api";
+import Task from "./components/Task";
 
 const App = () => {
-  return <div className="App">Task Tracker</div>;
+  const [tasks, setTasks] = useState([]);
+
+  // Fetch Tasks on page load
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      console.log(tasksFromServer);
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">Tasks</h1>
+      {tasks.length > 0 ? (
+        tasks.map((task) => <Task task={task} key={task.id} />)
+      ) : (
+        <h3>No Tasks To Show</h3>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,48 @@
+const endpoint = "http://localhost:3000";
+
+// READ
+const fetchTasks = async () => {
+  return fetch(`${endpoint}/tasks`)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
+
+// CREATE
+const addTask = async (task) => {
+  return fetch(`${endpoint}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
+
+// DELETE
+const deleteTask = async (taskId) => {
+  return fetch(`${endpoint}/tasks/${taskId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
+
+// UPDATE
+const updateTask = async (taskId) => {
+  return fetch(`${endpoint}/tasks/${task.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
+
+export { fetchTasks, addTask, deleteTask, updateTask };
