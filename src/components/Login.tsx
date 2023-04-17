c8c51071f1bcd24631f78d63d279851c5c452c9c
nateazevedo <nathanielazevedo@hotmail.com>
2023-04-17 16:33:01 -0700

    style clean up

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -66,11 +66,18 @@ const Login = () => {
     <ThemeProvider theme={theme}>
       <div className="login-container">
         <div className="login-info">
-          <h1>Task Tracker</h1>
+          <h1
+            style={{
+              fontSize: "75px",
+            }}
+          >
+            Task Tracker
+          </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptates, quod, quia, voluptate quae voluptatem quibusdam
-            necessitatibus quas voluptatum quidem nesciunt. Quisquam, quae
+            With an intuitive user interface and powerful features, Task Tracker
+            is designed to help you stay organized and focused. You can easily
+            create and manage tasks, set due dates, and even assign priority
+            levels to ensure you're tackling the most important tasks first.
           </p>
         </div>
         <div className="login-form-container">
@@ -78,12 +85,13 @@ const Login = () => {
             <h1
               style={{
                 color: "#0b1929",
+                fontSize: "50px",
               }}
             >
               {formType === "login" ? "Login" : "Sign Up"}
             </h1>
             {formType === "login" ? (
-              <FormControl>
+              <FormControl fullWidth>
                 <TextField
                   label="Username"
                   sx={{ color: "black", marginTop: "20px" }}
@@ -105,14 +113,18 @@ const Login = () => {
                 />
                 <Button
                   variant="contained"
-                  sx={{ marginTop: "20px" }}
+                  sx={{
+                    marginTop: "20px",
+                    height: "50px",
+                    backgroundColor: "#0b1929",
+                  }}
                   onClick={handleSubmit}
                 >
                   Login
                 </Button>
               </FormControl>
             ) : (
-              <FormControl>
+              <FormControl fullWidth>
                 <TextField
                   label="Username"
                   sx={{ color: "black", marginTop: "20px" }}
@@ -134,7 +146,11 @@ const Login = () => {
                 />
                 <Button
                   variant="contained"
-                  sx={{ marginTop: "20px" }}
+                  sx={{
+                    marginTop: "20px",
+                    height: "50px",
+                    backgroundColor: "#0b1929",
+                  }}
                   onClick={handleSubmit}
                 >
                   SignUP
